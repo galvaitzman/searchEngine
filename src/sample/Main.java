709628e81078a9342d411a7b910d1438d80ec3b8
gvaitzma <galvaitzman@gmail.com>
2018-12-01 14:15:16 +0200

    GUI gal

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,16 +2,20 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 import sun.awt.Mutex;
-
+import javafx.scene.image.Image;
+import java.awt.*;
 import java.io.*;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.*;
+import java.util.List;
 import java.util.zip.GZIPOutputStream;
 
 public class Main extends Application {
@@ -20,17 +24,120 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-    /*
+        //Image i = new Image("reasources/photo.png") ;
+        //ImageView imageView = new ImageView(i);
+        //Group root= new Group();
+        //root.getChildren().addAll(imageView);
+
         this.primaryStage = primaryStage;
         primaryStage.setTitle("Welcome to World-cup Maze");
         FXMLLoader fxmlLoader = new FXMLLoader();
         Parent root = fxmlLoader.load(getClass().getResource("Sample.fxml").openStream());
-        Scene scene = new Scene(root, 800, 300);
-        // scene.getStylesheets().add(getClass().getResource("WelcomeStyle.css").toExternalForm());
+        Scene scene = new Scene(root, 937, 618);
+     //   scene.getStylesheets().add(getClass().getResource("sample.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
-    }*/
+    }
+
+    public void startBuild(boolean isStemming,String pathOfCorpusAndStopWord , String postingAndDictionary) {
+        Parse parser = new Parse(pathOfCorpusAndStopWord,isStemming);
+        ReadFile readFile = new ReadFile(pathOfCorpusAndStopWord);
+
+        List<Pair<String, String>> readyDocumentsFromReadFile = readFile.documents;
+
+        BufferedWriter bufferWriter1 = null;
+        try {
+            bufferWriter1 = new BufferedWriter(new FileWriter(postingAndDictionary + "/docInfoCityLanguageHeadLine.txt",true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedWriter bufferWriter2 = null;
+        try {
+            bufferWriter2 = new BufferedWriter(new FileWriter(postingAndDictionary + "/docInfoFrequencyNumberOfUniqueWords.txt",true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long strt = System.nanoTime();
+        Indexer indexer = new Indexer(postingAndDictionary);
+        int i=0;
+        for (; i < readFile.filesInFolder.size() / 50 + 1; i += 1) {
+            System.out.println(i + " start");
+            readFile.read();
+            BufferedWriter finalBufferWriter = bufferWriter1;
+            Thread t1 = new Thread(() -> {
+                try {
+                    finalBufferWriter.write(readFile.stringBuilder.toString());
+                } catch (IOException e) {
+                }
+                System.out.println("finish writer1");
+            });
+            t1.start();
+
+            parser.startParsing50Files(readyDocumentsFromReadFile);
+            readFile.documents.clear();
+            parser.termsIndoc.clear();
+            BufferedWriter finalBufferWriter1 = bufferWriter2;
+            Thread t2 = new Thread(() -> {
+                try {
+                    finalBufferWriter1.write(parser.docInfo.toString());
+                } catch (IOException e) {
+                }
+                System.out.println("finish writer 2");
+            });
+            t2.start();
+
+            indexer.index50Files(parser.docsByTerm ,i);
+            parser.docsByTerm.clear();
+            //
+            //}
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println((System.nanoTime() - strt) * Math.pow(10, -9));
+        } //5-6 minutes
+        try {
+            bufferWriter1.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            bufferWriter2.flush();
+        } catch (IOException e) {
+
+        }
+        try {
+            bufferWriter1.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            bufferWriter2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        indexer.mergePost("/big"); //8 seconds
+        System.out.println((System.nanoTime() - strt) * Math.pow(10, -9));
+        indexer.mergePost("/small");//40 seconds
+        System.out.println((System.nanoTime() - strt) * Math.pow(10, -9));
+        indexer.mergeBigWithSmall(); //15 sseconds
+        System.out.println((System.nanoTime() - strt) * Math.pow(10, -9));
+        indexer.writeToFinalPosting(); // 1 minute
+        System.out.println((System.nanoTime() - strt) * Math.pow(10, -9));
+        indexer.writeDictionary();//4 seconds
+        System.out.println((System.nanoTime() - strt) * Math.pow(10, -9));
+
+    }
+    /*
         boolean isStemming = true;
         String corpusAndStopWordsPath = System.getProperty("user.dir") + "/src/reasources/corpus";
         String postingAndDictionary = System.getProperty("user.dir") + "/src/reasources";
--- src/sample/MainViewController.java
+++ src/sample/MainViewController.java
@@ -6,35 +6,75 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Optional;
 
 public class MainViewController extends Application{
 
     Main main = new Main();
     Stage primaryStage;
+    public TextField textBrowseStopWordAndCorpus;
+    public TextField textPathToSave;
+    public javafx.scene.control.CheckBox checkBoxStem;
+    public javafx.scene.image.ImageView imageView;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        this.primaryStage = primaryStage;
-        primaryStage.setTitle("Welcome to World-cup Maze");
+      /*  this.primaryStage = primaryStage;
+        primaryStage.setTitle("Serach engine");
         FXMLLoader fxmlLoader = new FXMLLoader();
-        Parent root = fxmlLoader.load(getClass().getResource("Welcome.fxml").openStream());
+        Parent root = fxmlLoader.load(getClass().getResource("sample.fxml").openStream());
          Scene scene = new Scene(root, 800, 300);
        // scene.getStylesheets().add(getClass().getResource("WelcomeStyle.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
 
-
+*/
         //--------------
     }
 
     public void About(ActionEvent actionEvent) {
       //  new AboutController().start();
     }
+
+ /*   public void browse(ActionEvent actionEvent) {
+        //  new AboutController().start();
+    }*/
+
+    public void startBuild(ActionEvent actionEvent) {
+
+        main.startBuild(checkBoxStem.isSelected(),textBrowseStopWordAndCorpus.getText(),textPathToSave.getText());
+    }
+
+
+    public void browse(ActionEvent actionEvent ){
+        DirectoryChooser dc = new DirectoryChooser();
+        dc.setTitle("Corpus and Stop word path");
+        File selectedFile = dc.showDialog(primaryStage);
+        if (selectedFile != null)
+            textBrowseStopWordAndCorpus.setText(selectedFile.getPath());
+    }
+    public void browse2(ActionEvent actionEvent ){
+        DirectoryChooser dc = new DirectoryChooser();
+        dc.setTitle("Save");
+        File selectedFile = dc.showDialog(primaryStage);
+        if (selectedFile != null)
+            textPathToSave.setText(selectedFile.getPath());
+    }
+
 
     public void NewGame(ActionEvent actionEvent) {
         try {
